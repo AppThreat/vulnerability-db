79dad1efdc1210142f1d6e53f3d22e32ea9278dc
Prabhu Subramanian <prabhu@appthreat.com>
2020-01-30 21:08:05 +0000

    Rewrite with msgpack

---
+++ test/test_storage.py
@@ -0,0 +1,43 @@
+import vulndb.lib.storage as storage
+from vulndb.lib.nvd import NvdSource
+from vulndb.lib.gha import GitHubSource
+
+import os
+
+import json
+import pytest
+
+import tempfile
+
+
+@pytest.fixture
+def test_vuln_data():
+    test_cve_data = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "data", "cve_data.json"
+    )
+    with open(test_cve_data, "r") as fp:
+        json_data = json.loads(fp.read())
+        nvdlatest = NvdSource()
+        return nvdlatest.convert(json_data)
+
+
+@pytest.fixture
+def test_gha_data():
+    test_cve_data = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "data", "gha_data.json"
+    )
+    with open(test_cve_data, "r") as fp:
+        json_data = json.loads(fp.read())
+        ghalatest = GitHubSource()
+        return ghalatest.convert(json_data)[0]
+
+
+def test_create(test_vuln_data):
+    with tempfile.NamedTemporaryFile(delete=False) as fp:
+        data = storage.store(test_vuln_data, db_file=fp.name)
+        assert data
+        fp.flush()
+
+        datas = storage.stream_read(db_file=fp.name)
+        assert len(datas) > len(test_vuln_data)
+        fp.close()
