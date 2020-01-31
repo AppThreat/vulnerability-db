a9f282986eb152cc35f79fd817c157094b12b0fb
Prabhu Subramanian <prabhu@appthreat.com>
2020-01-31 12:27:29 +0000

    Temp commit

---
+++ test/test_utils.py
@@ -0,0 +1,31 @@
+import vulndb.lib.utils as utils
+
+
+def test_normalise():
+    res = utils.normalise_num(100, 3)
+    assert res == 100
+
+    res = utils.normalise_num(100, 4)
+    assert res == 1000
+
+    res = utils.normalise_num(100, 2)
+    assert res == 100
+
+
+def test_version_compare():
+    res = utils.version_compare("3.0.0", "2.0.0", "2.7.9.4")
+    assert not res
+    res = utils.version_compare("2.0.0", "2.0.0", "2.7.9.4")
+    assert res
+    res = utils.version_compare("2.7.0", "2.0.0", "2.7.9.4")
+    assert res
+    res = utils.version_compare("2.7.9.4", "2.0.0", "2.7.9.4")
+    assert res
+    res = utils.version_compare("2.7.9.5", "2.0.0", "2.7.9.4")
+    assert not res
+    res = utils.version_compare("1.0.0", "2.0.0", "2.7.9.4")
+    assert not res
+    res = utils.version_compare("4.0.0", "2.0.0", "2.7.9.4")
+    assert not res
+    res = utils.version_compare("3.7.9.4", "2.0.0", "*")
+    assert res
